62a3b5f6bb3b0492217da83938f27196ddfa886c
anovasoftware <jburke@anovasoftware.com>
2025-03-05 21:47:01 +0000

    setup db 1

---
+++ .platform/hooks/postdeploy/run_migrations.cmd
@@ -0,0 +1,16 @@
+@echo off
+echo Running Django Migrations and Loading Data...
+cd /d C:\cfn\cfn-init  REM Change to the correct path of your Django app
+
+REM Activate virtual environment
+call C:\Python39\Scripts\activate.bat  REM Adjust this to match your venv path
+
+REM Run migrations
+python manage.py migrate static
+python manage.py migrate base
+
+REM Load data
+python manage.py loaddata static.json
+python manage.py loaddata base.json
+
+echo Migrations and Data Load Complete!
