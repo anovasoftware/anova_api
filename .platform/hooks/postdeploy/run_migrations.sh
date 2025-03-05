f3d76fd1049325630bcf43576637bd35079d5edc
anovasoftware <jburke@anovasoftware.com>
2025-03-05 23:28:23 +0000

    setup db 4

---
+++ .platform/hooks/postdeploy/run_migrations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Running Django Migrations and Loading Data..."
+
+# Ensure the script exits if any command fails
+set -e
+
+# Navigate to the application directory
+cd /var/app/current
+
+# Activate the virtual environment
+source ~/venv/bin/activate
+
+# Run migrations
+python manage.py migrate static
+python manage.py migrate base
+
+# Load data
+python manage.py loaddata static.json
+python manage.py loaddata base.json
+
+echo "Migrations and Data Load Complete!"
---
+++ .platform/hooks/predeploy/set_permissions.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chmod +x /var/app/staging/.platform/hooks/postdeploy/run_migrations.sh
