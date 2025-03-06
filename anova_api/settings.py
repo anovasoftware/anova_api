4017c097b1d58351732a081c1803e3dffb289bf6
anovasoftware <jburke@anovasoftware.com>
2025-03-06 18:20:06 +0000

    setup db 7

--- anova_api/settings.py
+++ anova_api/settings.py
@@ -34,10 +34,11 @@ ALLOWED_HOSTS = [
     "[::1]",
     "localhost:4200",
     "127.0.0.1:4200",
-    'anova-api-env.eba-hesbuejd.us-east-1.elasticbeanstalk.com',
+    # 'anova-api-env.eba-hesbuejd.us-east-1.elasticbeanstalk.com',
     'api.anovasea.net'
+    "34.233.187.33",  # Add your Elastic Beanstalk instance's IP
+    ".elasticbeanstalk.com",  # Allow AWS EB subdomains
 ]
-
 
 # Application definition
 
