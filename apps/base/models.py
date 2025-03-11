900791737a6fa80578c8bc7ed35b93d436737a06
anovasoftware <jburke@anovasoftware.com>
2025-03-11 16:04:26 +0000

    bridge 3

--- apps/base/models.py
+++ apps/base/models.py
@@ -7,6 +7,36 @@ from core.models import BaseModel
 # from .models_extended import *
 
 
+# AUTOGEN_BEGIN_Category#
+class Category(BaseModel):
+    category_id         = models.CharField(max_length=  5, blank=False, unique=True , primary_key=True )
+    client              = models.ForeignKey("static.Client", on_delete=models.CASCADE, related_name='+', default='999')
+    type                = models.ForeignKey("static.Type", on_delete=models.CASCADE, related_name="+", default='000')
+    status              = models.ForeignKey("static.Status", on_delete=models.CASCADE, related_name="+", default='001')
+    parent_category     = models.ForeignKey("self", on_delete=models.CASCADE, to_field='category_id', related_name="+", db_column="parent_category_id", default=None, null=True)
+    order_by            = models.CharField(max_length=  2, blank=False, unique=False, primary_key=False, default='99')
+    description         = models.CharField(max_length= 90, blank=False, unique=False, primary_key=False)
+    start_date          = models.DateTimeField(default=beginning_of_time)
+    end_date            = models.DateTimeField(default=end_of_time)
+    effective_status    = models.ForeignKey("static.Status", on_delete=models.CASCADE, related_name="+", db_column="effective_status_id", default='021')
+    grouping            = models.CharField(max_length= 20, blank=False, unique=False, primary_key=False, default='')
+    category_key        = models.CharField(max_length= 50, blank=False, unique=False, primary_key=False)
+    external_id         = models.CharField(max_length= 50, blank=False, unique=False, primary_key=False)
+    static_flag         = models.CharField(max_length=  1, blank=True , unique=False, primary_key=False, default='N')
+    internal_comment    = models.TextField(blank=True , unique=False, primary_key=False)
+    created_date        = models.DateTimeField(auto_now_add=True)
+    last_updated        = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table            = 'base_category'
+        verbose_name_plural = 'general categories (base_category)'
+        ordering            = []
+        
+    def __str__(self):
+        return 'category'
+# AUTOGEN_END_Category#
+
+
 # AUTOGEN_BEGIN_Identifier#
 class Identifier(models.Model):
     identifier_id    = models.CharField(max_length= 40, blank=False, unique=True , primary_key=True )
--- constants/type_constants.py
+++ constants/type_constants.py
@@ -1,6 +1,7 @@
 # AUTO GENERATED
 #  ALL
-NOT_APPLICABLE = '000'                                           # not applicableTO_BE_ANNOUNCED = '999'                                          # to be announced#  base_person
+NOT_APPLICABLE = '000'                                           # not applicableTO_BE_ANNOUNCED = '999'                                          # to be announced#  base_category
+BASE_CATEGORY_ROOM_CABIN = '020'                                 # room/cabin category#  base_person
 PERSON_REGULAR = '012'                                           # regular person#  base_person.gender
 GENDER_FEMALE = '702'                                            # femaleGENDER_MALE = '701'                                              # maleGENDER_NOT_APPLICABLE = '700'                                    # not applicableGENDER_NOT_DISCLOSED = '704'                                     # not disclosed#  res_hotel
 HOTE_CRUISE_SHIP = '006'                                         # cruise shipHOTE_HOTEL = '005'                                               # hotel#  static_client
