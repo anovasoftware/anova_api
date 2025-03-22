36130d9501db105f14cf98b0f5be44f441bc1b61
anovasoftware <jburke@anovasoftware.com>
2025-03-22 14:25:04 +0000

    reservation 2

---
+++ constants/company_constants.py
@@ -0,0 +1,3 @@
+# AUTO GENERATED
+#  Static Companies
+NOT_APPLICABLE = 'A0000'                                         # not applicableTO_BE_ANNOUNCED = 'A9999'                                        # to be announced
