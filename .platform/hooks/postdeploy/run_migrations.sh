e04da6c28e2a4217251a4db25bc1a3bd7603eba9
anovasoftware <jburke@anovasoftware.com>
2025-04-11 20:03:16 +0000

    chart field 8

--- .platform/hooks/postdeploy/run_migrations.sh
+++ .platform/hooks/postdeploy/run_migrations.sh
@@ -12,20 +12,20 @@ cd /var/app/current
 source /var/app/venv/staging-LQM1lest/bin/activate
 
 
-python manage.py migrate bridge
 # Run migrations
-#python manage.py migrate authtoken
-#python manage.py migrate static
-#python manage.py migrate base
-#python manage.py migrate res
+python manage.py migrate authtoken
+python manage.py migrate static
+python manage.py migrate base
+python manage.py migrate res
+#python manage.py migrate bridge
 
 
 # Load data
-#python manage.py loaddata static.json
-#python manage.py loaddata static.json
-#python manage.py loaddata base.json
-#python manage.py loaddata res.json
-##python manage.py loaddata bridge.json
-#python manage.py loaddata authtoken_token.json
+python manage.py loaddata static.json
+python manage.py loaddata static.json
+python manage.py loaddata base.json
+python manage.py loaddata res.json
+#python manage.py loaddata bridge.json
+python manage.py loaddata authtoken_token.json
 
 echo "Migrations and Data Load Complete!"
