27416cf5508607a8b61a30fa3adf3ea79e8c36a0
anovasoftware <jburke@anovasoftware.com>
2025-04-12 11:55:09 +0000

    chart field 8

--- .platform/hooks/postdeploy/run_migrations.sh
+++ .platform/hooks/postdeploy/run_migrations.sh
@@ -17,7 +17,7 @@ python manage.py migrate authtoken
 python manage.py migrate static
 python manage.py migrate base
 python manage.py migrate res
-#python manage.py migrate bridge
+python manage.py migrate bridge
 
 
 # Load data
@@ -25,7 +25,7 @@ python manage.py loaddata static.json
 python manage.py loaddata static.json
 python manage.py loaddata base.json
 python manage.py loaddata res.json
-#python manage.py loaddata bridge.json
+python manage.py loaddata bridge.json
 python manage.py loaddata authtoken_token.json
 
 echo "Migrations and Data Load Complete!"
