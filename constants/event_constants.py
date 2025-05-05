9feb750922e157154fa0921097386b99efa0cf91
anovasoftware <jburke@anovasoftware.com>
2025-05-05 21:30:51 +0000

    misc

---
+++ constants/event_constants.py
@@ -0,0 +1,3 @@
+# AUTO GENERATED
+#  ALL
+NOT_APPLICABLE = 'A00000'                                        # not applicableTO_BE_ANNOUNCED = 'A99999'                                       # to be announced
