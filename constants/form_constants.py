691c39abc04066685b2b27c655598ace7eb43c99
anovasoftware <jburke@anovasoftware.com>
2025-07-07 20:39:00 +0000

    login

---
+++ constants/form_constants.py
@@ -0,0 +1,3 @@
+# AUTO GENERATED
+#  Global
+LOGIN = '001'                                                    # loginNOT_APPLICABLE = '000'                                           # not applicable
