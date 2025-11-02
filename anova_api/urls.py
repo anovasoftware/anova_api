8a3003c622a93a190e3817b6244209bac828fecd
anovasoftware <jburke@anovasoftware.com>
2025-11-02 07:23:42 -0500

    render-setup.5

--- anova_api/urls.py
+++ anova_api/urls.py
@@ -5,13 +5,19 @@ from django.urls import path, include
 from core.api_views.core_api import health_check
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.http import JsonResponse
+from constants import  constants
 # urls.py
+
+def ping(request):
+    return JsonResponse({'status': 'OK', 'message': f'API connection successful version={constants.VERSION}'})
 
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path('ping/', ping, name='ping'),
     path("health/", health_check),
     path('api/v1/public/', include('core.urls.urls_public')),
     path('admin/', admin.site.urls),
