d0ff3fb140179a55792046e917b63eb5d70d7cd2
anovasoftware <jburke@anovasoftware.com>
2026-09-07 08:48:30 -0400

    BDS.3

---
+++ constants/room_constants.py
@@ -0,0 +1,3 @@
+# AUTO GENERATED
+#  ALL
+GUARANTEE = 'A002'                                               # guaranteeNOT_APPLICABLE = 'A000'                                          # not applicableTO_BE_ANNOUNCED = 'A999'                                         # room not selectedWAITLIST = 'A001'                                                # waitlist
